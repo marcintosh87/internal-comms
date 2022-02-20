40c2f71570e03e85f762ea3d43ab6664607009c5
Marcos <marcintosh87@gmail.com>
2022-02-20 13:34:44 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  resources :news_comments
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ 
+    resources :news_comments
   resources :event_comments
   resources :event_posts
   resources :news_posts
   resources :users
   resources :departments
   resources :organizations
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    get "/me", to: "users#show"
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+ 
+
+# this will allow for the deployment to us react
+get '*path',
+to: 'fallback#index',
+constraints: ->(req) { !req.xhr? && req.format.html? }
 end
