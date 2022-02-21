1824057577b9a7bde2414bd858c2e02f0baba6f9
Marcos <marcintosh87@gmail.com>
2022-02-21 09:28:49 -0500

    configged key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  ENV["1e45dd95c11dbc9c6779ab086466cb5a"]
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
